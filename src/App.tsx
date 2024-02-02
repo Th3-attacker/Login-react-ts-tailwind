bd164c3f22e068b407b9e8e04ed9bb33941424af
Th3-attacker <haje.malik5@gmail.com>
2024-02-02 10:40:07 +0000

    feat: :package: radix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 // import Login from "./components/Login";
-import { motion } from "framer-motion";
+
 function App() {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      className=" mx-auto w-full h-screen min-w-min  flex flex-col justify-center place-items-center"
-    >
-      {/* <Login /> */}
-    </motion.div>
-  );
+  return <div className="">{/* <Login /> */}</div>;
 }
 
 export default App;
